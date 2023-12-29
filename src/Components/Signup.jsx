ce96022c1dd18939a22d0165364eff8b5b5c6893
Oyakhire Aisosa Margaret <aisosagraphicdesign@gmail.com>
2023-12-29 18:22:33 +0100

    "third commit"

--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -1,124 +1,125 @@
 import React from 'react'
-import axios from "../axios/axios-create";
-import PropTypes from "prop-types";
-import { useParams } from "react-router-dom";
-import { createBrowserHistory } from "history";
+// import axios from "../axios/axios-create";
+// import PropTypes from "prop-types";
+// import { useParams } from "react-router-dom";
+// import { createBrowserHistory } from "history";
 
 
 
-axios.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
+// axios.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("token");
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return config;
+// });
 
-const LoginForm = () => {
-  const history = createBrowserHistory();
-  const { userEmail } = useParams();
-  const [email, setYourEmail] = useState(userEmail);
-  const [password, setYourPassword] = useState("");
-  const [showPassword, setShowPassword] = useState(false);
+// const LoginForm = () => {
+//   const history = createBrowserHistory();
+//   const { userEmail } = useParams();
+//   const [email, setYourEmail] = useState(userEmail);
+//   const [password, setYourPassword] = useState("");
+//   const [showPassword, setShowPassword] = useState(false);
 
-  const [googleReg, setGoogleReg] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const [errorMessage, setErrorMessage] = useState("");
-  const [successMessage, setSuccessMessage] = useState("");
-  const [token, setToken] = useState();
+//   const [googleReg, setGoogleReg] = useState(false);
+//   const [loading, setLoading] = useState(false);
+//   const [errorMessage, setErrorMessage] = useState("");
+//   const [successMessage, setSuccessMessage] = useState("");
+//   const [token, setToken] = useState();
 
-  // if (!token) {
-  //   return <Signin setToken={setToken} />;
-  // }
+//   // if (!token) {
+//   //   return <Signin setToken={setToken} />;
+//   // }
 
-  const handleShowPassword = () => {
-    setShowPassword(!showPassword);
-  };
+//   const handleShowPassword = () => {
+//     setShowPassword(!showPassword);
+//   };
 
-  const navigate = useNavigate();
+//   const navigate = useNavigate();
 
-  const handleClick = async (e) => {
-    e.preventDefault();
-    if (!email.trim() || !password.trim()) {
-      setErrorMessage("Please provide both email and password.");
-      return;
-    }
+//   const handleClick = async (e) => {
+//     e.preventDefault();
+//     if (!email.trim() || !password.trim()) {
+//       setErrorMessage("Please provide both email and password.");
+//       return;
+//     }
 
-    const form = {
-      email,
-      password,
-    };
+//     const form = {
+//       email,
+//       password,
+//     };
 
-    setLoading(true);
+//     setLoading(true);
 
-    try {
-      const response = await axios.post("/auth/login", JSON.stringify(form), {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
+//     try {
+//       const response = await axios.post("/auth/login", JSON.stringify(form), {
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//       });
 
-      setToken(response.data.tokens.access.token);
-      localStorage.setItem("token", response.data.tokens.access.token);
-      // I negated the response to email verification because of blockage
-      if (response.data.user.isEmailVerified) {
-        setSuccessMessage("Email Verified.");
+//       setToken(response.data.tokens.access.token);
+//       localStorage.setItem("token", response.data.tokens.access.token);
+//       // I negated the response to email verification because of blockage
+//       if (response.data.user.isEmailVerified) {
+//         setSuccessMessage("Email Verified.");
 
-        // navigate("/medication");
-      } else {
-        // alert("False");
-        setErrorMessage("Please verify your email first! Redirecting...");
-        setTimeout(() => {
-          // navigate(`/emailverification/${email}`);
-          navigate('/medication');
-        }, 1000);
-      }
-      setLoading(false);
-      localStorage.setItem("userId", response.data.user.id);
-      // setSuccessMessage(true);
-      if (response.data.success) {
-        localStorage.setItem("user", JSON.stringify(response.data.user));
-        // navigate("/medication");
-      } else {
-        setErrorMessage(response.data.message);
-      }
-      localStorage.setItem("user", response.data.user);
-      localStorage.setItem("token", response.data.tokens.access.token);
-    } catch (error) {
-      if (!error?.response) {
-        setErrorMessage("Network Error! Try Again.");
-      } else if (error.response?.status === 400) {
-        setErrorMessage("Unauthorized");
-      } else if (error.response?.status === 401) {
-        setErrorMessage("Incorrect Email or Password");
-      } else if (response.data.message.toLowerCase().includes("email")) {
-        setErrorMessage("email", response.data.message);
-      } else if (!token) {
-        setErrorMessage("Please verify your email.");
-      } else {
-        console.error("Error:", error);
-        setErrorMessage("An error occurred. Please try again.");
-      }
-    } finally {
-      setLoading(false);
-      // navigate("/medication");
-      // setTimeout(() => {
-      //   navigate("/medication");
-      // }, 2000);
-      // history.push(`/emailverification/${email}`);
-    }
-  };
+//         // navigate("/medication");
+//       } else {
+//         // alert("False");
+//         setErrorMessage("Please verify your email first! Redirecting...");
+//         setTimeout(() => {
+//           // navigate(`/emailverification/${email}`);
+//           navigate('/medication');
+//         }, 1000);
+//       }
+//       setLoading(false);
+//       localStorage.setItem("userId", response.data.user.id);
+//       // setSuccessMessage(true);
+//       if (response.data.success) {
+//         localStorage.setItem("user", JSON.stringify(response.data.user));
+//         // navigate("/medication");
+//       } else {
+//         setErrorMessage(response.data.message);
+//       }
+//       localStorage.setItem("user", response.data.user);
+//       localStorage.setItem("token", response.data.tokens.access.token);
+//     } catch (error) {
+//       if (!error?.response) {
+//         setErrorMessage("Network Error! Try Again.");
+//       } else if (error.response?.status === 400) {
+//         setErrorMessage("Unauthorized");
+//       } else if (error.response?.status === 401) {
+//         setErrorMessage("Incorrect Email or Password");
+//       } else if (response.data.message.toLowerCase().includes("email")) {
+//         setErrorMessage("email", response.data.message);
+//       } else if (!token) {
+//         setErrorMessage("Please verify your email.");
+//       } else {
+//         console.error("Error:", error);
+//         setErrorMessage("An error occurred. Please try again.");
+//       }
+//     } finally {
+//       setLoading(false);
+//       // navigate("/medication");
+//       // setTimeout(() => {
+//       //   navigate("/medication");
+//       // }, 2000);
+//       // history.push(`/emailverification/${email}`);
+//     }
+//   };
 
-  useEffect(() => {
-    setErrorMessage("");
-  }, [email, password]);
+//   useEffect(() => {
+//     setErrorMessage("");
+//   }, [email, password]);
 
-  useEffect(() => {
-    setGoogleReg(false);
-  }, [email, password]);
+//   useEffect(() => {
+//     setGoogleReg(false);
+//   }, [email, password]);
 
-
-    return (
+function Signup() {
+  return (
+  
         <div>
 
 <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
@@ -162,8 +163,5 @@ const LoginForm = () => {
     )
 }
 
-LoginForm.propTypes = {
-  setToken: PropTypes.func.isRequired,
-};
 
 export default Signup
